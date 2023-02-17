759adcca92ca9478b178f0c84a575d753d79f5d6
Artem Tishchenko <82469944+artem20210413@users.noreply.github.com>
2023-02-17 20:02:28 +0200

    -

--- Homework_7/hellel-laravel/app/Models/Post.php
+++ Homework_7/hellel-laravel/app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -15,9 +16,10 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  * @property string comment
  */
 class Post extends Model
-
 {
-    public $table = 'post';
+    use HasFactory;
+
+    protected $table = 'post';
 
     protected $fillable = [
         'id',
